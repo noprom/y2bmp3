e474934615dfadbbd9ec5d28a09a857491e2342e
sfo01 <sfo01>
2017-07-13 11:51:56 +0000

    modify redis

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	_ "y2bmp3/routers"
-
+	
+	"time"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/cache"
 	_ "github.com/astaxie/beego/cache/redis"
 	_ "github.com/astaxie/beego/session/redis"
 )
 
-var bm, err := cache.NewCache("redis", `{"key":"collectionName","conn":"redis:6379","dbNum":"0","password":""}`)
-if err != nil {
-	beego.Error("connect to redis error" + err)
-}
+var bm, err = cache.NewCache("redis", `{"key":"collectionName","conn":"redis:6379","dbNum":"0","password":""}`)
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
@@ -20,7 +18,8 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 
-	bm.Put("astaxie", 1, 10*time.Second)
+	beego.Debug("Gegin Run")
+	bm.Put("astaxie", 1, 3600*time.Second)
 	bm.Get("astaxie")
 	bm.IsExist("astaxie")
 
