644b605d5cc555e41f8c243bc63db9296b18063d
noprom <tyee.noprom@qq.com>
2017-07-14 21:06:17 +0800

    rm cache

--- main.go
+++ main.go
@@ -4,24 +4,14 @@ import (
 	_ "y2bmp3/routers"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/cache"
-	_ "github.com/astaxie/beego/cache/redis"
 	_ "github.com/astaxie/beego/session/redis"
-	"time"
 )
-
-var bm, err = cache.NewCache("redis", `{"key":"collectionName","conn":"redis:6379","dbNum":"0","password":""}`)
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-
-	beego.Debug("Gegin Run")
-	bm.Put("astaxie", 1, 3600*time.Second)
-	bm.Get("astaxie")
-	bm.IsExist("astaxie")
 
 	beego.SetLogger("file", `{"filename":"logs/run.log"}`)
 	beego.Run()
